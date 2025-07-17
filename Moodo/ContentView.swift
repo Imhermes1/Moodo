19ccf437cf61b93039f5fb9f9a1b8793b8a71969
Luke <lukeforn@hotmail.com>
2025-07-18 09:34:44 +1000

    Initial Commit

---
+++ Moodo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moodo
+//
+//  Created by Luke Fornieri on 18/7/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moodo/MoodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodoApp.swift
+//  Moodo
+//
+//  Created by Luke Fornieri on 18/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
